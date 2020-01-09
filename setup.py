8f8d543a261bc06e9beeb0a87beb524a8c59834e
Maciej Lenartowicz <maciej.lenartowicz@nic.cz>
2020-01-09 17:56:56 +0100

    Updated Foris JS to 3.0.0.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class CustomBuild(build_py):
 
 setuptools.setup(
     name=NAME,
-    version='2.2.1',
+    version='2.2.2',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 

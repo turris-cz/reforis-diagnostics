30d57caed1a39fe1db6d1dbfe26b728388031adc
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-03-12 10:11:35 +0100

    Build via Hatch

--- reforis_static/__init__.py
+++
@@ -1,6 +0,0 @@
-#  Copyright (C) 2019-2023 CZ.NIC z.s.p.o. (https://www.nic.cz/)
-#
-#  This is free software, licensed under the GNU General Public License v3.
-#  See /LICENSE for more information.
-
-__import__('pkg_resources').declare_namespace(__name__)
